558fc75db9cd595a8452f1c7c7cdc3d98f4c5e91
BrainJarProductions <vgrilli1@gmail.com>
2020-07-12 17:37:41 -0400

    fix icon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/pizza-shark-only-sm.png',
+  manifestIcon: 'src/assets/images/pizza-shark-only-sm.png',
   pathPrefix: `/pizza-shark/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Pizza Shark',
   subHeading: 'At the Pleasent Lake General Store',
