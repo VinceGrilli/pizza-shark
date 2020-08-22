b58a30eca704bed319d312c6f246e308667f31e5
BrainJarProductions <vgrilli1@gmail.com>
2020-08-22 12:37:58 -0400

    add btc-logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import logo from '../assets/images/btc-logo.png';
 export default function Footer() {
   return (
     <footer className="footer text-faded text-center py-4">
       <div className="container">
-        <p className="m-0 small">
-          © Copyright {new Date().getFullYear()} All rights reserved. Website
-          crafted by{' '}
-          <a className="text-warning" href="https://vincegrilli.dev/">
-            Vince Grilli
+        <p className="m-0 ">
+          © {new Date().getFullYear()}, Site crafted by
+          {` `}
+          <a href="https://www.builttocode.dev">
+            <img
+              style={{
+                maxWidth: `7rem`,
+              }}
+              src={logo}
+              alt="Built to Code"
+            />
           </a>
         </p>
       </div>
