9f0613745b83c356781cabdbf0d0894b11ded6e8
BrainJarProductions <vgrilli1@gmail.com>
2021-12-02 09:20:26 -0500

    add module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Modal from "react-bootstrap/Modal"
 import { Link } from 'gatsby';
 import { useState } from 'react';
 import fbicon from '../assets/images/Facebook.svg'
@@ -9,85 +9,84 @@ const Header = () => {
   
   const [menuOpen, setMenuOpen] = useState(false)
   const [activeLink, setActiveLink] = useState('Home')
-    
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+  
     return (
       <nav
         className="navbar fixed-top navbar-expand-lg navbar-dark py-lg-2"
         id="mainNav"
       >
+        <div className="col-10 col-sm-auto text-left flex-sm-row">
         
-          <div className="col-10 col-sm-auto text-left flex-sm-row">
-          <a
+          <button className="btn btn-link navbar-brand text-uppercase sm-text-left font-weight-bold " onClick={showModal}>Order Online</button>
+          <Modal show={isOpen} size="lg" onHide={hideModal}>
+            <Modal.Header>
+              <Modal.Title>Choose Location</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>The body</Modal.Body>
+          </Modal>
+        </div>
+        <div className="col-2 col-sm-auto ">
+          <button
+            onClick={() => {setMenuOpen(!menuOpen)}}
+            className={`navbar-toggler justify-content-end  ${menuOpen ? '' : 'collapsed'}`}
             type="button"
-            className="btn btn-link navbar-brand text-uppercase sm-text-left font-weight-bold "
-            href="/harwich"
+            aria-controls="navbarResponsive"
+            aria-expanded={menuOpen}
+            aria-label="Toggle navigation"
           >
-            Order Harwich
-          </a>
-          <a
-            type="button"
-            className={`btn btn-link navbar-brand text-uppercase font-weight-bold `}
-            href="/chatham"
-          >
-            Order Chatham
-          </a>
-          </div>
-          <div className="col-2 col-sm-auto ">
-            <button
-              onClick={() => {setMenuOpen(!menuOpen)}}
-              className={`navbar-toggler justify-content-end  ${menuOpen ? '' : 'collapsed'}`}
-              type="button"
-              aria-controls="navbarResponsive"
-              aria-expanded={menuOpen}
-              aria-label="Toggle navigation"
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div
+          className={`collapse navbar-collapse  ${menuOpen ? 'show' : ''}`}
+          id="navbarResponsive"
+        >
+          <ul className="navbar-nav text-center mx-auto ">
+            <li
+              className={`nav-item px-lg-3 ${
+                activeLink === 'Home' ? '' : setActiveLink('Home')
+              }`}
             >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-          <div
-            className={`collapse navbar-collapse  ${menuOpen ? 'show' : ''}`}
-            id="navbarResponsive"
-          >
-            <ul className="navbar-nav text-center mx-auto ">
-              <li
-                className={`nav-item px-lg-3 ${
-                  activeLink === 'Home' ? '' : setActiveLink('Home')
-                }`}
+              <Link className={`nav-link text-uppercase text-expanded ${menuOpen ? 'pt-3' : ''}`} to="/">
+                Home
+              </Link>
+            </li>
+            <li
+              className={`nav-item px-lg-2 ${
+                activeLink === 'store' ? 'active' : ''
+              }`}
+            >
+              <Link
+                className="nav-link text-uppercase text-expanded"
+                to="/hours"
               >
-                <Link className={`nav-link text-uppercase text-expanded ${menuOpen ? 'pt-3' : ''}`} to="/">
-                  Home
-                </Link>
-              </li>
-              <li
-                className={`nav-item px-lg-2 ${
-                  activeLink === 'store' ? 'active' : ''
-                }`}
-              >
-                <Link
-                  className="nav-link text-uppercase text-expanded"
-                  to="/hours"
-                >
-                  Hours
-                </Link>
-              </li>
-            </ul>
-            <div className="navbar-end mx-2 text-center">
-              
-                <a href="https://www.facebook.com/PleasantLakePizza" title="facebook" className=" rounded-full inline-block transition-shadow duration-300 hover:shadow-2xl">
-                  <img src={fbicon} alt="facebook" className="social-item"/>
-                </a>
-              
-              
-                <a href="https://www.instagram.com/pizzasharkcc/" title="instagram" className="mr-2 rounded-full inline-block transition-shadow duration-300 hover:shadow-2xl">
-                  <img src={instaicon} alt="instagram" className="social-item"/>
-                </a>
-              
-            </div>
+                Hours
+              </Link>
+            </li>
+          </ul>
+          <div className="navbar-end mx-2 text-center">
+            
+              <a href="https://www.facebook.com/PleasantLakePizza" title="facebook" className=" rounded-full inline-block transition-shadow duration-300 hover:shadow-2xl">
+                <img src={fbicon} alt="facebook" className="social-item"/>
+              </a>
+            
+            
+              <a href="https://www.instagram.com/pizzasharkcc/" title="instagram" className="mr-2 rounded-full inline-block transition-shadow duration-300 hover:shadow-2xl">
+                <img src={instaicon} alt="instagram" className="social-item"/>
+              </a>
+            
           </div>
-          
-        
-      </nav>
-    );
+        </div>
+    </nav>
+  );
 }
 
 export default Header
