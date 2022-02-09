e50b2a43c58230edb6f8e9b79479b39bb15af5b3
BrainJarProductions <vgrilli1@gmail.com>
2022-02-08 21:19:34 -0500

    add article link and newest newsletter

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,31 @@ const IndexPage = () => (
               <div className="bg-faded rounded p-5">
                 <h2 className="section-heading mb-4">
                   <span className="section-heading-lower text-center mb-3">
+                    just voted top 5 pizza places on cape by cape cod times readers!
+                  </span>
+                </h2>
+                <div className="intro-button text-center mt-4">
+                  <a
+                    className="btn btn-primary btn-xl mb-2"
+                    href="https://amp.capecodtimes.com/amp/9289846002"
+                  >
+                    Click here to read the article
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="page-section about-heading">
+      <div className="container">
+        <div className="about-heading-content mt-5 mb-3 ">
+          <div className="row">
+            <div className="col-xl-9 col-lg-10 mx-auto">
+              <div className="bg-faded rounded p-5">
+                <h2 className="section-heading mb-4">
+                  <span className="section-heading-lower text-center mb-3">
                     pizza shark at the pleasant lake general store
                   </span>
                 </h2>
@@ -21,13 +46,13 @@ const IndexPage = () => (
                 Located on the Brewster-Harwich line, directly on the Cape Cod Rail Trail. Pizza, salads, sandwiches & ice cream year-round. Excellent location for a quick & easy lunch. Grocery essentials, snacks, beverages, beer & wine available in the General Store. Please use our hassle-free online ordering system to earn rewards points. We offer delivery 7 nights a week to all of Brewster and Harwich and you can order everything from the general store along with your pizzas. One-stop shopping! 
                 </p>
                 <div className="intro-button text-center mt-4">
-                    <a
-                      className="btn btn-primary btn-xl mb-4"
-                      href="https://www.toasttab.com/pizza-shark-403-pleasant-lake-ave"
-                    >
-                      Order Harwich
-                    </a>
-                  </div>
+                  <a
+                    className="btn btn-primary btn-xl mb-4"
+                    href="https://www.toasttab.com/pizza-shark-403-pleasant-lake-ave"
+                  >
+                    Order Harwich
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -87,36 +112,41 @@ const IndexPage = () => (
                 </span>
               </h2>
               <ul className="list-unstyled list-hours mb-5 text-center mx-auto">
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="text-center" href="https://mailchi.mp/5524968395ae/the-holidays-are-coming-and-we-are-ready?e=[UNIQID]">
-                      The holidays are coming and we are READY!
-                    </a>
-                  </li>
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="" href="https://mailchi.mp/ff11e35637fe/fall-is-here-wings-are-back?e=[UNIQID]">
-                      Fall is here & wings are BACK!
-                    </a>
-                  </li>
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="text-center" href="https://mailchi.mp/68c966f7d50b/hark-hark-its-pizza-shark-wines?e=[UNIQID]">
-                      Hark! Hark! It's Pizza Shark... wines?!
-                    </a>
-                  </li>
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="text-center" href="https://mailchi.mp/56947d0915bc/swimming-into-summer-2021?e=[UNIQID]">
-                      Swimming into Summer 2021!
-                    </a>
-                  </li>
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="text-center" href="https://mailchi.mp/940c7993220d/pizza-shark-the-little-red-store-is-now-open?e=[UNIQID]">
-                      PIZZA SHARK! The little red store is now OPEN!
-                    </a>
-                  </li>
-                  <li className="list-unstyled-item list-hours-item d-flex">
-                    <a className="text-center" href="https://mailchi.mp/70be2e1da639/welcome-to-the-shark-squad?e=[UNIQID]">
-                      Welcome to the Shark Squad! 
-                    </a>
-                  </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/54529d672fc8/updates-from-harwich-and-chatham">
+                    Updates from Harwich AND Chatham
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/5524968395ae/the-holidays-are-coming-and-we-are-ready?e=[UNIQID]">
+                    The holidays are coming and we are READY!
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="" href="https://mailchi.mp/ff11e35637fe/fall-is-here-wings-are-back?e=[UNIQID]">
+                    Fall is here & wings are BACK!
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/68c966f7d50b/hark-hark-its-pizza-shark-wines?e=[UNIQID]">
+                    Hark! Hark! It's Pizza Shark... wines?!
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/56947d0915bc/swimming-into-summer-2021?e=[UNIQID]">
+                    Swimming into Summer 2021!
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/940c7993220d/pizza-shark-the-little-red-store-is-now-open?e=[UNIQID]">
+                    PIZZA SHARK! The little red store is now OPEN!
+                  </a>
+                </li>
+                <li className="list-unstyled-item list-hours-item d-flex">
+                  <a className="text-center" href="https://mailchi.mp/70be2e1da639/welcome-to-the-shark-squad?e=[UNIQID]">
+                    Welcome to the Shark Squad! 
+                  </a>
+                </li>
               </ul>
               <div className="row px-5 mt-5">
                 <div className="intro-button mx-auto">
@@ -142,8 +172,7 @@ const IndexPage = () => (
                   >
                     Gift Cards
                   </a>
-                </div>
-                
+                </div>                
               </div>
             </div>
           </div>
