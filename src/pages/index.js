c4d3c1b800127bb6c3534468af01fb6bd2a07650
VinceGrilli <vgrilli1@gmail.com>
2024-02-19 18:06:31 -0500

    add embeded video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+import { Container } from 'react-bootstrap';
 import store from '../assets/images/store-inside.jpg';
 import orpheum from '../assets/images/orpheum.jpg';
 import dennisStore from '../assets/images/dennis-village.jpeg';
@@ -9,6 +10,13 @@ import dennisStore from '../assets/images/dennis-village.jpeg';
 const IndexPage = () => {
   return (
   <Layout activeLink="home">
+    <section className='page-section'>
+      <Container>
+        <div className="embed-responsive embed-responsive-16by9">
+          <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/sWQTVIuzUcI?si=iQiKI5WUARDWJtso" title="Pizza Shark Commercial" allowFullScreen></iframe>
+        </div>
+      </Container>
+    </section>
     <section className="page-section about-heading">
       <div className="container">
         <div className="about-heading-content mt-5 ">
